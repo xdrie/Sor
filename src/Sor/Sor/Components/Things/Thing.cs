e94b0582e574394fdc414a9368234ce202ebe804
xdrie <xdrie@alt.icu>
2020-01-22 12:03:37 -0800

    make capsule a Thing

---
+++ src/Sor/Sor/Components/Things/Thing.cs
@@ -0,0 +1,8 @@
+using Glint.Sprites;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sor.Components.Things {
+    public class Thing : GAnimatedSprite {
+        public Thing(Texture2D texture, int width, int height) : base(texture, width, height) { }
+    }
+}
