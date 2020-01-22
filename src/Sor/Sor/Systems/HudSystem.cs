6e0445ef34c71c256d2a72560cdbcf709a8e8bb5
xdrie <xdrie@alt.icu>
2020-01-21 21:23:38 -0800

    solve energy display definition todo

--- src/Sor/Sor/Systems/HudSystem.cs
+++ src/Sor/Sor/Systems/HudSystem.cs
@@ -11,10 +11,11 @@ namespace Sor.Systems {
             this.player = player;
             energyIndicator = hudNt.GetComponent<IndicatorBar>();
         }
-        
+
         public override void Process() {
-            // TODO: use better-defined numbers
-            energyIndicator.setValue(Mathf.Clamp((float) player.core.ratio, 0, 1));
+            if (player.core != null) {
+                energyIndicator.setValue(Mathf.Clamp((float) player.core.ratio, 0, 1));
+            }
         }
     }
 }
