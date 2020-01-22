054432c8d459377127587f3f5e7d51592b331550
xdrie <xdrie@alt.icu>
2020-01-22 14:07:22 -0800

    add mono release

--- script/build_mono_release.sh
+++ script/build_mono_release.sh
@@ -2,23 +2,25 @@ set -e
 
 pushd .
 
+PROJECT=Sor
 FRAMEWORK=net472
+PROJECT_PATH=./src/$PROJECT/${PROJECT}Dk
 
 REVISION=$(git log --pretty=format:'%h' -n 1)
 ARCNAME="sor_mono-$REVISION"
 ARTIFACT="builds/$ARCNAME.7z"
 
 echo "Running MSBuild..."
-cd src/Sor
-msbuild Sor.sln /p:Configuration=Release
+cd src/$PROJECT
+msbuild $PROJECT.sln /p:Configuration=Release
 
 popd
 
 echo "Copying natives..."
-cp natives/* src/Sor/SorDk/bin/Release/net471/
+cp natives/* $PROJECT_PATH/bin/Release/$FRAMEWORK/
 
 mkdir -p builds/
 echo "Compressing to $ARTIFACT..."
-7z a $ARTIFACT ./src/Sor/SorDk/bin/Release/net471/*
+7z a $ARTIFACT $PROJECT_PATH/bin/Release/$FRAMEWORK/*
 
 echo "Release built!"
