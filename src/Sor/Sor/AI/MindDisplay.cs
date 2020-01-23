1d2ffc3f5cfb7b77d305c8f437b4f8b260f8264c
xdrie <xdrie@alt.icu>
2020-01-23 08:02:21 -0800

    add debug and draw flags to mind

--- src/Sor/Sor/AI/MindDisplay.cs
+++ src/Sor/Sor/AI/MindDisplay.cs
@@ -10,38 +10,47 @@ namespace Sor.AI {
         private Mind mind;
         private Wing wing;
         private Color textCol = Core.Services.GetService<GameContext>().assets.fgColor;
+        private bool draw;
 
-        public MindDisplay(Wing player) {
+        public MindDisplay(Wing player, bool draw) {
             this.player = player;
+            this.draw = draw;
         }
 
         public override void OnAddedToEntity() {
             base.OnAddedToEntity();
 
             mind = Entity.GetComponent<Mind>();
+            mind.debug = true; // enable trace debug
             wing = mind.me;
         }
 
+        public override void OnRemovedFromEntity() {
+            base.OnRemovedFromEntity();
+
+            mind.debug = false; // disable trace debug
+        }
+
         public override RectangleF Bounds {
-            get {
-                return Entity.Scene.Camera.Bounds;
-            }
+            get { return Entity.Scene.Camera.Bounds; }
         }
 
         public override void Render(Batcher batcher, Camera camera) {
-            // TODO: show information about whose mind, etc.
-            // TODO: draw the info
-            StringBuilder ind = new StringBuilder();
-            ind.AppendLine($"[mind] {wing.name}");
-            ind.AppendLine($"vision: {mind.state.seenWings.Count} | {mind.state.seenThings.Count}");
-            ind.AppendLine($"opinion: {mind.state.getOpinion(player.mind)}");
-            batcher.DrawString(Graphics.Instance.BitmapFont, ind, 
-                camera.ScreenToWorldPoint(new Vector2(20, 20)), textCol);
+            if (draw) {
+                // TODO: show information about whose mind, etc.
+                // draw mind info representation
+                StringBuilder ind = new StringBuilder();
+                ind.AppendLine($"[mind] {wing.name}");
+                ind.AppendLine($"vision: {mind.state.seenWings.Count} | {mind.state.seenThings.Count}");
+                ind.AppendLine($"opinion: {mind.state.getOpinion(player.mind)}");
+                batcher.DrawString(Graphics.Instance.BitmapFont, ind,
+                    camera.ScreenToWorldPoint(new Vector2(20, 20)), textCol);
+            }
         }
 
         public override void DebugRender(Batcher batcher) {
             base.DebugRender(batcher);
-            
+
             // sensor rect
             batcher.DrawHollowRect(new Rectangle(mind.visionSystem.sensorRec.Location.ToPoint(),
                 mind.visionSystem.sensorRec.Size.ToPoint()), Color.Green);
