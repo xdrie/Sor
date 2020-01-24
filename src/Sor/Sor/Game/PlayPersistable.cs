bcee9bf1b638eb52c23ca8013e50b06367df445d
xdrie <xdrie@alt.icu>
2020-01-23 22:54:55 -0800

    don't save player in wing list

--- src/Sor/Sor/Game/PlayPersistable.cs
+++ src/Sor/Sor/Game/PlayPersistable.cs
@@ -12,10 +12,10 @@ namespace Sor.Game {
     public class PlayPersistable : IPersistable {
         private PlayScene play;
         private PlaySceneSetup setup;
-        
+
         public bool loaded = false;
         public const int version = 2;
-        
+
         // default values
         public Vector2 playerPosition = new Vector2(200, 200);
 
@@ -29,9 +29,10 @@ namespace Sor.Game {
             Global.log.writeLine($"{nameof(PlayPersistable)}::recover called", GlintLogger.LogLevel.Information);
             var readVersion = rd.ReadInt();
             if (version != readVersion) {
-                Global.log.writeLine($"save file version mismatch (got {readVersion}, expected {version})", GlintLogger.LogLevel.Error);
+                Global.log.writeLine($"save file version mismatch (got {readVersion}, expected {version})",
+                    GlintLogger.LogLevel.Error);
             }
-            
+
             // read player
             var playerWd = rd.readWingMeta();
             play.playerWing.name = playerWd.name;
@@ -51,13 +52,15 @@ namespace Sor.Game {
         public void Persist(IPersistableWriter wr) {
             Global.log.writeLine($"{nameof(PlayPersistable)}::persist called", GlintLogger.LogLevel.Information);
             wr.Write(version); // save file version
-            
+
             // save player
             wr.writeWingMeta(play.playerWing);
             wr.writeFromBody(play.playerWing.body);
-            
+
             // save all other wings
-            var wingsToSave = play.FindEntitiesWithTag(Constants.ENTITY_WING).ToList();
+            var wingsToSave = play.FindEntitiesWithTag(Constants.ENTITY_WING)
+                .Where(x => x != play.playerEntity)
+                .ToList();
             wr.Write(wingsToSave.Count);
             foreach (var wingNt in wingsToSave) {
                 var wing = wingNt.GetComponent<Wing>();
