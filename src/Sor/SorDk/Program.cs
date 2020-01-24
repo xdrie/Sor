4a263d88780777f7aa0a4aa96d100939fd5d72ea
xdrie <xdrie@alt.icu>
2020-01-23 16:05:07 -0800

    use resources for default config

--- src/Sor/SorDk/Program.cs
+++ src/Sor/SorDk/Program.cs
@@ -1,28 +1,25 @@
 ﻿using System.IO;
+using System.Reflection;
 using Sor;
 using Sor.Game;
 
 namespace SorDk {
     class Program {
         public const string conf = "game.conf";
+
         static void Main(string[] args) {
             var config = new Config();
+#if DEBUG
+            var defaultConf = Assembly.GetExecutingAssembly().GetManifestResourceStream("SorDk.Res.game.dbg.conf");
+#else
+            var defaultConf = Assembly.GetExecutingAssembly().GetManifestResourceStream("SorDk.Res.game.conf");
+#endif
             if (!File.Exists(conf)) {
-                File.WriteAllText(conf, @"
-# game config file
-
-[video]
-w=960
-h=540
-fullscreen=false
-scaleMode=0
-framerate=60
-maxVfx=true
-
-[platform]
-logLevel=3
-");
+                using (var sr = new StreamReader(defaultConf)) {
+                    File.WriteAllText(conf, sr.ReadToEnd());
+                }
             }
+
             var confStr = File.ReadAllText(conf);
             config.read(confStr); // load and parse config
             using (var game = new NGame(config)) {
