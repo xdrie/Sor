e7594fe4304c2b778e6996a3a7dc024f2ce8ebfa
xdrie <xdrie@alt.icu>
2020-01-23 20:36:35 -0800

    add capsule tweens

--- src/Sor/Sor/Components/Things/Capsule.cs
+++ src/Sor/Sor/Components/Things/Capsule.cs
@@ -10,10 +10,14 @@ namespace Sor.Components.Things {
         public float firstAvailableAt = 0;
         public const float lifetime = 20f;
         public float despawnAt = 0;
-        
+
         public CapsuleBody body;
         public Wing sender = null;
         public Tree creator = null;
+
+        private const float tweenDur = 0.4f;
+        private static Color fadeColor = new Color(100, 100, 200, 100);
+        private static Color defColor = Color.White;
 
         public Capsule() : base(Core.Content.LoadTexture("Data/sprites/nrg.png"), 16, 16) {
             animator.AddAnimation("default", new[] {sprites[0], sprites[1], sprites[2], sprites[3]});
@@ -24,12 +28,13 @@ namespace Sor.Components.Things {
         public override void Initialize() {
             base.Initialize();
 
+            spriteRenderer.Color = fadeColor;
             despawnAt = Time.TotalTime + lifetime;
 
             body = Entity.AddComponent<CapsuleBody>();
             Entity.AddComponent(new BoxCollider(-8, -12, 8, 24) {Tag = Constants.COLLIDER_THING, IsTrigger = true});
             Entity.AddComponent(new BoxCollider(-40, -40, 80, 80) {Tag = Constants.TRIGGER_GRAVITY, IsTrigger = true});
-            
+
             // use slow updates
             UpdateInterval = 10;
         }
@@ -57,21 +62,28 @@ namespace Sor.Components.Things {
         public void acquire() {
             if (!acquired) {
                 energy = 0;
-                var tw = spriteRenderer.TweenColorTo(Color.Black, 0.4f);
+                var tw = spriteRenderer.TweenColorTo(Color.Black, tweenDur);
                 tw.SetCompletionHandler(t => { Entity?.Destroy(); });
                 tw.Start();
+                spriteRenderer.Transform.TweenLocalScaleTo(0.5f, tweenDur).Start();
             }
+
             acquired = true;
         }
 
         public void Update() {
+            // check color
+            if (Time.TotalTime >= firstAvailableAt - tweenDur) {
+                spriteRenderer.TweenColorTo(defColor, tweenDur).Start();
+            }
+
             // update animation speed based on energy
             var animSpeed = Mathf.Clamp(energy / 400f, 0.5f, 2f);
             animator.Speed = animSpeed;
             // check despawn
             if (Time.TotalTime > despawnAt) {
-                Enabled = false;
-                Entity.Destroy();
+                // Enabled = false;
+                acquire();
             }
         }
     }
