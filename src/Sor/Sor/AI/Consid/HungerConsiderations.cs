4b7f61f491ac0cdece0aeb93517bc82811bf4fc6
xdrie <xdrie@alt.icu>
2020-01-31 16:30:57 -0800

    add hungerappraisal implementation

--- src/Sor/Sor/AI/Consid/HungerConsiderations.cs
+++ src/Sor/Sor/AI/Consid/HungerConsiderations.cs
@@ -1,13 +1,21 @@
 using System;
+using Glint.AI.Misc;
 using LunchtimeGears.AI.Utility;
+using Sor.Components.Things;
 
 namespace Sor.AI.Consid {
     public class HungerConsiderations {
         public class HungerAppraisal : Appraisal<Mind> {
             public HungerAppraisal(Mind context) : base(context) { }
-
+            
             public override float score() {
-                throw new NotImplementedException();
+                // hunger score is based on the necessity of more energy.
+                // let E be energy percentage (energy / max energy), clamp01
+                // y = (1 - E)^2
+                var energyCore = context.Entity.GetComponent<EnergyCore>();
+                var energyPercentage = (float) (energyCore.energy / energyCore.designMax);
+                energyPercentage = Gmathf.clamp01(energyPercentage);
+                return energyPercentage * energyPercentage;
             }
         }
 
