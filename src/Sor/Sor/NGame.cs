64d70b1784539ecb07a7db773694d26eb6376757
xdrie <xdrie@alt.icu>
2020-02-05 00:18:41 -0800

    bump version

--- src/Sor/Sor/NGame.cs
+++ src/Sor/Sor/NGame.cs
@@ -11,7 +11,7 @@ using Sor.Scenes;
 namespace Sor {
     public class NGame : GlintCore {
         public const string GAME_TITLE = "Sor";
-        public const string GAME_VERSION = "0.5.15.0301-dev";
+        public const string GAME_VERSION = "0.5.16.0502-dev";
 
         private readonly GameContext gameContext;
 
