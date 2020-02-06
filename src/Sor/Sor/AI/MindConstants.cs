8558ccd9332ab3f7e9da35a4023cfd8ee017a743
xdrie <xdrie@alt.icu>
2020-02-05 18:33:54 -0800

    decrease nearby position size

--- src/Sor/Sor/AI/MindConstants.cs
+++ src/Sor/Sor/AI/MindConstants.cs
@@ -2,7 +2,7 @@ namespace Sor.AI {
     public class MindConstants {
         public const int SENSE_RANGE = 800;
 
-        public const float NEARBY_POSITION_SQ = 10f * 10f;
+        public const float NEARBY_POSITION_SQ = 4f * 4f;
 
         public const int OPINION_DESPISE = -500;
         public const int OPINION_HATE = -300;
