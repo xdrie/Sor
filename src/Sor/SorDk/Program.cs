ba4c787cf44872935f7317274510ec32060a6725
xdrie <xdrie@alt.icu>
2020-03-03 12:52:51 -0800

    add critical crash cradle

--- src/Sor/SorDk/Program.cs
+++ src/Sor/SorDk/Program.cs
@@ -2,7 +2,9 @@
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using Glint;
 using Glint.Config;
+using Glint.Util;
 using Sor;
 using Sor.Game;
 
@@ -15,19 +17,19 @@ namespace SorDk {
             using (var sr = new StreamReader(banner)) {
                 Console.WriteLine(sr.ReadToEnd());
                 Console.WriteLine(NGame.GAME_VERSION);
-                #if DEBUG
+#if DEBUG
                 Console.WriteLine("[DEBUG] build, debug code paths enabled. maim mode enabled.");
-                #endif
+#endif
             }
-            
-            #if DEBUG
+
+#if DEBUG
             // check MAIM (MAintenance IMmediate access) mode
             if (args.Length > 0 && args[0] == "maim") {
                 Maim.launch(args.Skip(1).ToList());
                 return;
             }
-            #endif
-            
+#endif
+
             // load configuration
 #if DEBUG
             var defaultConf = Assembly.GetExecutingAssembly().GetManifestResourceStream("SorDk.Res.game.dbg.conf");
@@ -38,8 +40,14 @@ namespace SorDk {
             configHelper.ensureDefaultConfig(conf, defaultConf);
             var confStr = File.ReadAllText(conf);
             var config = configHelper.load(confStr, args); // load and parse config
-            using (var game = new NGame(config)) {
+            // run in crash-cradle
+            try {
+                using var game = new NGame(config);
                 game.Run();
+            }
+            catch (Exception ex) {
+                Global.log.writeLine($"fatal error: {ex}", GlintLogger.LogLevel.Critical);
+                throw;
             }
         }
     }
