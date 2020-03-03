c23f1a5a4935dd7896fe3ea27233e0cd51a635e9
xdrie <xdrie@alt.icu>
2020-03-03 14:07:03 -0800

    add arctype option

--- script/build_mono_release.sh
+++ script/build_mono_release.sh
@@ -6,6 +6,16 @@ PROJECT=Sor
 FRAMEWORK=net472
 PROJECT_PATH=./src/$PROJECT/${PROJECT}Dk
 NATIVES_PATH=$(pwd)/natives
+DEBUG=0
+if [ -n "$1" ] && [ "$1" = "debug" ]; then
+    echo "setting DEBUG=1"
+    DEBUG=1
+fi
+ARCTYPE=7z
+if [ -n "$2" ]; then
+    echo "setting ARCTYPE=$2"
+    ARCTYPE=$2
+fi
 
 PARSE_VERSION=$(grep 'GAME_VERSION' ./src/$PROJECT/$PROJECT/NGame.cs | cut -d \" -f2)
 REVISION=$(git tag -l --points-at HEAD)
@@ -14,17 +24,25 @@ if [ -z "${REVISION}" ]; then
     REVISION="${PARSE_VERSION}_${GIT_REVISION}"
 fi
 ARCNAME="sor_mono-v$REVISION"
-ARTIFACT="builds/$ARCNAME.7z"
+
+CONFIGURATION=Release
+if [[ $DEBUG -eq 1 ]];
+then
+CONFIGURATION=Debug
+ARCNAME="${ARCNAME}_dbg"
+fi
+
+ARTIFACT="builds/$ARCNAME.$ARCTYPE"
 
 cd src/$PROJECT
 echo "running restore..."
 dotnet restore $PROJECT.Mono.sln
 echo "running build..."
-msbuild $PROJECT.Mono.sln /p:Configuration=Release
+msbuild $PROJECT.Mono.sln /p:Configuration=$CONFIGURATION
 
 popd
 
-OUTPATH=$PROJECT_PATH/bin/Release/$FRAMEWORK
+OUTPATH=$PROJECT_PATH/bin/$CONFIGURATION/$FRAMEWORK
 echo "copying natives..."
 cp $NATIVES_PATH/* $OUTPATH/
 cp script/${PROJECT}_game $OUTPATH/
