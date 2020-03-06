15ed28ad2cf9bdf04153a7b2613a69f98e108900
xdrie <xdrie@alt.icu>
2020-03-06 01:37:14 -0800

    build full sng

--- src/Sor/Sor/AI/Nav/StructuralNavigationGraph.cs
+++ src/Sor/Sor/AI/Nav/StructuralNavigationGraph.cs
@@ -1,15 +1,37 @@
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using Sor.Game;
 
 namespace Sor.AI.Nav {
     public class StructuralNavigationGraph {
         public const int DOOR_NODE_DIST = 2;
+        public List<Node> nodes;
+
+        public StructuralNavigationGraph(List<Node> nodes) {
+            this.nodes = nodes;
+        }
+        
         public class Node {
             public Point pos;
             public List<Node> links;
+            public RoomLink edge;
 
             public Node(Point pos) {
                 this.pos = pos;
+            }
+        }
+        
+        public class RoomLink {
+            public Map.Room r1;
+            public Map.Room r2;
+
+            public RoomLink(Map.Room r1, Map.Room r2) {
+                this.r1 = r1;
+                this.r2 = r2;
+            }
+
+            public bool equiv(RoomLink other) {
+                return (r1 == other.r1 && r2 == other.r2) || (r1 == other.r2 && r2 == other.r1);
             }
         }
     }
--- src/Sor/Sor/AI/Nav/StructuralNavigationGraphBuilder.cs
+++ src/Sor/Sor/AI/Nav/StructuralNavigationGraphBuilder.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Xna.Framework;
 using Sor.Game;
 using Sor.Util;
@@ -24,7 +25,10 @@ namespace Sor.AI.Nav {
                 // recursively collapse all pending nodes
                 foreach (var link in pendingLinks) {
                     link.collapse(); // collapse this child link
-                    centerNode.links.Add(link.centerNode); // attach the child link to me (center)
+                    // attach the child link to me (center)
+                    // (two-way)
+                    centerNode.links.Add(link.centerNode); // add them to my links
+                    link.centerNode.links.Add(centerNode); // add me to their links
                 }
             }
         }
@@ -34,15 +38,15 @@ namespace Sor.AI.Nav {
         }
 
         public StructuralNavigationGraph build() {
-            return null;
+            var nodeList = sngNodes.Values.Select(x => x.centerNode).ToList();
+            return new StructuralNavigationGraph(nodeList);
         }
 
         public void analyze() {
             // 1. build mapping from room to center nodes
             foreach (var room in roomGraph.rooms) {
                 // create unconnected center nodes
-                sngNodes[room] = new DelayedNode(
-                    new StructuralNavigationGraph.Node(room.center));
+                sngNodes[room] = new DelayedNode(new StructuralNavigationGraph.Node(room.center));
             }
 
             // 2. create nodes of indirection
@@ -58,15 +62,63 @@ namespace Sor.AI.Nav {
                     var outerDoor = doorCenter + new Point(dx * StructuralNavigationGraph.DOOR_NODE_DIST,
                         dy * StructuralNavigationGraph.DOOR_NODE_DIST);
                     var innerDoorNode = new DelayedNode(new StructuralNavigationGraph.Node(innerDoor));
-                    var outerDoorNode = new DelayedNode(new StructuralNavigationGraph.Node(outerDoor));
+                    var outerDoorNode = new DelayedNode(new StructuralNavigationGraph.Node(outerDoor) {
+                        edge = new StructuralNavigationGraph.RoomLink(door.roomLocal, door.roomOther)
+                    });
                     centerNode.addPendingLink(innerDoorNode); // attach CENTER - INNER
                     innerDoorNode.addPendingLink(outerDoorNode); // attach INNER - OUTER
                     // when collapsed, we should get CENTER - INNER - OUTER (a "spike")
                 }
             }
+
             // now, we have all our nodes of the form
             // CENTER with [INNER - OUTER] spikes for each door
-            // next, we need to merge these spikes by door link
+            // 3. next, we need to merge these spikes by door link
+            // and merge all these delayed nodes into a graph
+            foreach (var nodePair in sngNodes) {
+                var room = nodePair.Key;
+                var centerNode = nodePair.Value;
+                var gn = centerNode.centerNode;
+
+                // do a BFS on the node. we are going to make a list of spike nodes.
+                var spikeNodes = new List<StructuralNavigationGraph.Node>();
+                var visited = new Dictionary<StructuralNavigationGraph.Node, bool>();
+                var queue = new Queue<StructuralNavigationGraph.Node>();
+                // mark the starting node as visited, and queue it
+                visited[gn] = true;
+                queue.Enqueue(gn);
+
+                while (queue.Count > 0) {
+                    // process the current vertex
+                    var vertex = queue.Dequeue();
+                    if (vertex.edge != null) {
+                        spikeNodes.Add(vertex);
+                    }
+
+                    // get all adjacent, and enqueue any unvisited
+                    foreach (var adjacent in vertex.links) {
+                        if (!visited.ContainsKey(adjacent) || !visited[adjacent]) { // not visited
+                            visited[adjacent] = true; // mark visited
+                            queue.Enqueue(adjacent); // enqueue
+                        }
+                    }
+                }
+
+                // then, we will x2 loop through all spike nodes and match spike to spike via link equiv
+                foreach (var spk1 in spikeNodes) {
+                    foreach (var spk2 in spikeNodes) {
+                        if (spk1 == spk2) continue;
+                        // now, match spike-to-spike
+                        if (spk1.edge.equiv(spk2.edge)) {
+                            // we have a match!
+                            spk1.links.Add(spk2);
+                            spk2.links.Add(spk1);
+                        }
+                    }
+                }
+
+                // this should give us a graph!
+            }
         }
     }
 }
