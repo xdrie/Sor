d33ed383c2a7f8e3736226b89af2524219bcaa96
xdrie <xdrie@alt.icu>
2020-03-06 17:46:01 -0800

    disable mono release in build all

--- script/build_all_release.sh
+++ script/build_all_release.sh
@@ -17,7 +17,7 @@ fi
 echo "building all (${ARGS})..."
 ./script/build.sh
 ./script/get_natives.sh
-./script/build_mono_release.sh
+# ./script/build_mono_release.sh
 ./script/build_native_release.sh linux-x64 ${ARGS}
 ./script/build_native_release.sh osx-x64 ${ARGS}
 ./script/build_native_release.sh win-x64 ${ARGS}
