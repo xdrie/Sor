87223cbad9391cec6bd0d02bca99ec8758db3a40
xdrie <xdrie@alt.icu>
2020-03-11 14:37:37 -0700

    bump version

--- src/Sor/Sor/NGame.cs
+++ src/Sor/Sor/NGame.cs
@@ -8,7 +8,7 @@ using Sor.Scenes;
 namespace Sor {
     public class NGame : RGameBase<Config, GameContext> {
         public const string GAME_TITLE = "Sor";
-        public const string GAME_VERSION = "0.6.13.09-dev";
+        public const string GAME_VERSION = "0.6.15.01-dev";
 
         public static GameContext context => Core.Services.GetService<GameContext>(); 
 
