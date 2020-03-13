8f27939174db187d88ba882cf9561d6ea163ee29
xdrie <xdrie@alt.icu>
2020-03-12 21:39:58 -0700

    candidate is null if no best

--- src/Sor/Sor/AI/Consid/SocialAppraisals.cs
+++ src/Sor/Sor/AI/Consid/SocialAppraisals.cs
@@ -21,6 +21,7 @@ namespace Sor.AI.Consid {
                 // TODO: de-prioritize ducks we're already chums with
                 var wings = mind.state.seenWings.MaxBy(
                     x => mind.state.getOpinion(x.mind) > thresh);
+                if (!wings.Any()) return null;
                 return wings.First();
             }
 
