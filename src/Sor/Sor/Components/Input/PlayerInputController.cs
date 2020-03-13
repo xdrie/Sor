18986b1d3fc2ce9e3804060d141e993484cd4ef5
xdrie <xdrie@alt.icu>
2020-03-13 12:45:42 -0700

    use more concise input setup methods

--- src/Sor/Sor/Components/Input/PlayerInputController.cs
+++ src/Sor/Sor/Components/Input/PlayerInputController.cs
@@ -5,26 +5,29 @@ namespace Sor.Components.Input {
     public class PlayerInputController : InputController {
         public override void Initialize() {
             base.Initialize();
-            
+
             // moveDirectionInput.Nodes.Add(new VirtualJoystick.KeyboardKeys(VirtualInput.OverlapBehavior.CancelOut,
             //     Keys.A, Keys.D, Keys.W, Keys.S));
-            moveDirectionInput.Nodes.Add(new VirtualJoystick.KeyboardKeys(VirtualInput.OverlapBehavior.CancelOut,
-                Keys.Left, Keys.Right, Keys.Up, Keys.Down));
-            moveDirectionInput.Nodes.Add(new VirtualJoystick.KeyboardKeys(VirtualInput.OverlapBehavior.CancelOut,
-                Keys.J, Keys.L, Keys.I, Keys.K));
-            moveDirectionInput.Nodes.Add(new VirtualJoystick.GamePadLeftStick());
+            moveDirectionInput
+                .AddKeyboardKeys(VirtualInput.OverlapBehavior.CancelOut,
+                    Keys.Left, Keys.Right, Keys.Up, Keys.Down);
+            moveDirectionInput
+                .AddKeyboardKeys(VirtualInput.OverlapBehavior.CancelOut,
+                    Keys.J, Keys.L, Keys.I, Keys.K);
             var gamepadIndex = 0;
+            moveDirectionInput.AddGamePadLeftStick(gamepadIndex);
+            // invert gamepad sticks to match virtual keyboard input
             Nez.Input.GamePads[gamepadIndex].IsLeftStickVerticalInverted = true;
             Nez.Input.GamePads[gamepadIndex].IsRightStickVerticalInverted = true;
-            
-            interactInput.Nodes.Add(new VirtualButton.KeyboardKey(Keys.E));
-            interactInput.Nodes.Add(new VirtualButton.GamePadButton(gamepadIndex, Buttons.Y));
-            
-            boostInput.Nodes.Add(new VirtualButton.KeyboardKey(Keys.LeftShift));
-            
-            fireInput.Nodes.Add(new VirtualButton.KeyboardKey(Keys.Space));
-            
-            tetherInput.Nodes.Add(new VirtualButton.KeyboardKey(Keys.D2));
+
+            interactInput.AddKeyboardKey(Keys.E);
+            interactInput.AddGamePadButton(gamepadIndex, Buttons.Y);
+
+            boostInput.AddKeyboardKey(Keys.LeftShift);
+
+            fireInput.AddKeyboardKey(Keys.Space);
+
+            tetherInput.AddKeyboardKey(Keys.D2);
         }
     }
 }
