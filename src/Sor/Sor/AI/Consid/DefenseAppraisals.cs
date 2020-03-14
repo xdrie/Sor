5fe26c554a33b1352598a25bd67249dadfb102ae
xdrie <xdrie@alt.icu>
2020-03-14 11:49:33 -0700

    add stub threatfightable based on prop ratios

--- src/Sor/Sor/AI/Consid/DefenseAppraisals.cs
+++ src/Sor/Sor/AI/Consid/DefenseAppraisals.cs
@@ -4,6 +4,7 @@ using LunchLib.Calc;
 using MoreLinq;
 using Nez;
 using Sor.Components.Units;
+using XNez.GUtils.Misc;
 
 namespace Sor.AI.Consid {
     public static class DefenseAppraisals {
@@ -53,9 +54,34 @@ namespace Sor.AI.Consid {
         public class ThreatFightable : Appraisal<Mind> {
             public ThreatFightable(Mind context) : base(context) { }
 
+            private float scoreRatio(float ratio) {
+                // this assumes that <1 is advantage and >1 is disadvantage
+                if (ratio < 1) return 1; // adv
+                if (ratio > 1) return -1; // disadv
+                return 0; // neutral
+            }
+
             public override float score() {
-                // TODO: determine how fightable the threats are
-                return 0.7f; // all threats are fightable now
+                // determine how fightable nearby threats are
+                var threat = NearbyThreat.greatestThreat(context);
+                // TODO: use better functions to map ratios to score
+                // 1. compare core sizes (ratio) -> transform [-40, 40]
+                var coreSizeRatio = context.me.core.designMax / threat.mind.me.core.designMax;
+                var coreSizeScore = scoreRatio(1 / coreSizeRatio) * 40f;
+                // 2. compare maneuverability (ratio) -> transform [-20, 20]
+                // if we're more maneuverable, it might not be worth fighting
+                var maneuverabilityRatio = context.me.body.turnPower / threat.mind.me.body.turnPower;
+                var maneuScore = scoreRatio(maneuverabilityRatio) * 20f;
+                // 3. compare speed (ratio) -> transform [-40, 40]
+                // if we're faster, we want to fight less
+                var speedRatio = context.me.body.thrustPower / threat.mind.me.body.thrustPower;
+                var speedScore = scoreRatio(speedRatio) * 40f;
+
+                // clamp score to [-100, 100] -> transform [0, 1]
+                var score = coreSizeScore + maneuScore + speedScore;
+                var clampedScore = GMathf.clamp(score, -100, 100);
+                var fightable = GMathf.map01(clampedScore, -100, 100);
+                return fightable;
             }
         }
     }
