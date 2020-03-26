6d18853a466faebc34804eddf689f8a2f22d1574
xdrie <xdrie@alt.icu>
2020-03-25 17:53:57 -0700

    bump version

--- src/Sor/Sor/NGame.cs
+++ src/Sor/Sor/NGame.cs
@@ -2,14 +2,13 @@ using Glint;
 using Glint.Branding;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Nez;
 using Sor.Game;
 using Sor.Scenes;
 
 namespace Sor {
     public class NGame : RGameBase<GameContext, Config> {
         public const string GAME_TITLE = "Sor";
-        public const string GAME_VERSION = "0.7.12";
+        public const string GAME_VERSION = "0.7.13";
 
         public NGame(Config config) : base(config, new GameContext(config), GAME_TITLE, new Point(960, 540)) { }
 
