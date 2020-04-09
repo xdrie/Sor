323a3038a19313f2677f7d9b35ba4d3f488c6d10
xdrie <xdrie@alt.icu>
2020-04-09 16:27:23 -0700

    strip build all

--- script/build_all_release.sh
+++ script/build_all_release.sh
@@ -2,24 +2,8 @@
 
 set -e
 
-ARGS=""
-PACK=0
-if [ -n "$1" ] && [ "$1" = "pack" ]; then
-    echo "setting PACK=1"
-    PACK=1
-    ARGS="${ARGS} pack"
-fi
-
-if [[ $PACK -eq 1 ]];
-then
-    echo "getting tools..."
-    ./script/get_tools.sh
-fi
-
 echo "building all (${ARGS})..."
 ./script/build.sh
-./script/get_natives.sh
-# ./script/build_mono_release.sh
 ./script/build_native_release.sh linux-x64 ${ARGS}
 ./script/build_native_release.sh osx-x64 ${ARGS}
 ./script/build_native_release.sh win-x64 ${ARGS}
