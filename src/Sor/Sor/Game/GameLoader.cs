520cac09c1b2adc90dd99f581bdb5fb4cd9b3158
xdrie <xdrie@alt.icu>
2020-04-26 15:44:08 -0700

    fixup! add stub for storing non-game save data

--- src/Sor/Sor/Game/GameLoader.cs
+++ src/Sor/Sor/Game/GameLoader.cs
@@ -1,11 +1,9 @@
-using Glint.Game;
-
 namespace Sor.Game {
     public static class GameLoader {
         public static PlayPersistable loadSave(PlayContext playContext) {
             var store = NGame.context.data.getStore();
             var pers = new PlayPersistable(playContext);
-            store.Load(GameData<Config>.TEST_SAVE, pers);
+            store.Load(Constants.Game.GAME_SLOT_0, pers);
             return pers;
         }
     }
