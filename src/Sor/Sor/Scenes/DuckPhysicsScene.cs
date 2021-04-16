7c4bb50611196cdc0b6c3d6aeb2a59f33f12add0
xdrie <xdrie@alt.icu>
2021-04-16 00:00:04 -0700

    fix duckphysics minddisplay

--- src/Sor/Sor/Scenes/DuckPhysicsScene.cs
+++ src/Sor/Sor/Scenes/DuckPhysicsScene.cs
@@ -15,7 +15,7 @@ namespace Sor.Scenes {
     public class DuckPhysicsScene : GameScene {
         private Entity physicistDuck;
         private Entity playerNt;
-        private const int renderlayer_ui_overlay = 1 << 30;
+        private const int renderlayer_overlay = 1 << 30;
 
         public override void Initialize() {
             base.Initialize();
@@ -27,7 +27,7 @@ namespace Sor.Scenes {
 
             // add fixed renderer
             var fixedRenderer =
-                AddRenderer(new ScreenSpaceRenderer(1023, renderlayer_ui_overlay));
+                AddRenderer(new ScreenSpaceRenderer(1023, renderlayer_overlay));
             fixedRenderer.ShouldDebugRender = false;
 
             playerNt = CreateEntity(Constants.Game.PLAYER_NAME, new Vector2(400, 400)).SetTag(Constants.Tags.WING);
@@ -41,9 +41,13 @@ namespace Sor.Scenes {
             var physicistSoul = new AvianSoul {ply = new BirdPersonality {A = 0.8f, S = -0.4f}};
             var duckWing = physicistDuck.AddComponent(new Wing(new DuckMind(physicistSoul, true)));
             physicistDuck.AddComponent<LogicInputController>();
-            physicistDuck.AddComponent(new MindDisplay(playerWing, true));
-            duckWing.core.energy = 1000f;
+            var md = physicistDuck.AddComponent(new MindDisplay(playerWing, true));
+            md.RenderLayer = renderlayer_overlay;
             duckWing.mind.state.addOpinion(playerWing, Constants.DuckMind.OPINION_FRIEND);
+            
+            // give both of them plenty of energy
+            playerWing.core.energy = 10000f;
+            duckWing.core.energy = 10000f;
 
             // set the player as the target
             duckWing.mind.state.plan.Enqueue(new EntityTarget(duckWing.mind, playerWing.Entity, Approach.Within,
@@ -66,10 +70,11 @@ namespace Sor.Scenes {
             }
 
             var wing = physicistDuck.GetComponent<Wing>();
-            if (Input.LeftMouseButtonPressed) {
+            if (Input.LeftMouseButtonPressed || (Input.GamePads.Length > 0 && Input.GamePads[0].IsButtonPressed(Buttons.LeftStick))) {
                 // set duck target to mouse pos
                 var mouseWp = Camera.ScreenToWorldPoint(Input.MousePosition);
-                wing.mind.state.plan.Enqueue(new FixedTarget(wing.mind, mouseWp, Approach.Within, TargetSource.RANGE_CLOSE));
+                wing.mind.state.plan.Enqueue(new FixedTarget(wing.mind, mouseWp, Approach.Within,
+                    TargetSource.RANGE_CLOSE));
             }
         }
     }
